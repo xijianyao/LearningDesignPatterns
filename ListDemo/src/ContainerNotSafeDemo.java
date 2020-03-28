a3f1cabb4e0ddf636df72cec629401d28cf41810
xijianyao <xijianyao@qq.com>
2020-03-28 18:53:44 +0800

    volatile

--- ListDemo/src/ContainerNotSafeDemo.java
+++ ListDemo/src/ContainerNotSafeDemo.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /*
  * 集合不安全问题
@@ -9,11 +10,13 @@ import java.util.*;
  * 解决方案：
  * 1.new Vector<>()
  * 2.Collections.synchronizedList(new ArrayList<>());
+ * 3.new CopyOnWriteArrayList<>();
+ *
  *
  * */
 public class ContainerNotSafeDemo {
     public static void main(String[] args) {
-        List<String> list = Collections.synchronizedList(new ArrayList<>());
+        List<String> list = new CopyOnWriteArrayList<>();
 
         for (int i = 0; i < 30; i++) {
             new Thread(() -> {
