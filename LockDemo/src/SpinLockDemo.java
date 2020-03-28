4df02b802b3cef790d06e82abfaac4456e904bd4
xijianyao <xijianyao@qq.com>
2020-03-28 23:36:34 +0800

    volatile

---
+++ LockDemo/src/SpinLockDemo.java
@@ -0,0 +1,46 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+//自旋锁的实现
+public class SpinLockDemo {
+    public static void main(String[] args) {
+        SpinLockDemo spinLockDemo = new SpinLockDemo();
+        new Thread(()->{
+            spinLockDemo.myLock();
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            spinLockDemo.myUnLock();
+        },"A").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(()->{
+            spinLockDemo.myLock();
+            spinLockDemo.myUnLock();
+        },"B").start();
+    }
+
+    //原子引用线程
+    AtomicReference<Thread> atomicReference = new AtomicReference<>();
+
+    public void myLock() {
+        Thread thread = Thread.currentThread();
+        System.out.println(Thread.currentThread().getName() + "\t come in ");
+        while (!atomicReference.compareAndSet(null, thread)) {
+
+        }
+    }
+
+    public void myUnLock(){
+        Thread thread = Thread.currentThread();
+        atomicReference.compareAndSet(thread,null);
+        System.out.println(Thread.currentThread().getName()+"\n invoked myUnLock()");
+    }
+}
